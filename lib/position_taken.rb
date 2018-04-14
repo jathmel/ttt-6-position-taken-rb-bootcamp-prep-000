092392aa6ca078651011e6a5e6f75cb7906210ae
Jathmel Gordon <jathmel@gmail.com>
2018-04-14 23:21:38 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, oindex)
-  if board != " "
-    true
-  else
+def position_taken?(board, index)
+  if board[index] != " "
     false
+  else
+    true
   end
 end
